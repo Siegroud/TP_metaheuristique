6ffd90b17c1e3b6b6a853d7c7aef5f6e2c5a126d
Maxence <roussel59330@gmail.com>
2024-01-04 20:34:09 +0100

    verif_parcour

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include "filereading.h"
+#include "filereading.c"
+#include "verif_sol.c"
 #include "headers.h"
 
 
@@ -12,4 +14,14 @@ int main() {
     size = read_from_file("data/bayg29_10_1.dat", &tab, &demand, &constraints);
     printf("%d\n", size);
     print_tab(size, tab);
+
+    int etape[4];
+    etape[0]=2;
+    etape[1]=3;
+    etape[2]=5;
+    etape[3]=5;
+
+    int test = verif_sol(tab, 0, 7, etape,4);
+    printf(" test verif : %d \n" ,test);
+
 }
---
+++ verif_sol.c
@@ -0,0 +1,20 @@
+#include <stdbool.h>
+
+int verif_sol (int **tab,int d,int a,int *etape,int nb_etape){// d=depart a=depot
+    int pos=d;//position actuelle
+    int i=0;
+
+    for (i=0; i<nb_etape; i++){//tant qu'on a pas parcour tt les etape
+        if (etape[i]>sizeof(tab) || etape[i]<0){// si on ne connait pas ce port
+            return 0;
+        }
+        else if (tab[pos][etape[i]]<=0){// si le deplacement de la position actuelle a la nouvelle etape est imposible
+            return 0;
+        }
+        else {// sinon c possible donc on avance
+            pos=etape[i];
+        }
+    }
+
+    return 1;
+};
